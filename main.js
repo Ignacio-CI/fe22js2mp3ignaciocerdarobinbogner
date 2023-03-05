cd67cfa22da6ccc7f1a24266cdc7b01cb77d48b6
Ignacio-CI <ignacio.cerda.i@gmail.com>
2023-03-05 09:31:30 +0100

    add firebase setup

---
+++ main.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js"
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBVGKXt0navmTtlydS0X82fEUJTqxxcvg8",
+
+  authDomain: "javascript2-mp3-store.firebaseapp.com",
+
+  databaseURL: "https://javascript2-mp3-store-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "javascript2-mp3-store",
+
+  storageBucket: "javascript2-mp3-store.appspot.com",
+
+  messagingSenderId: "619437220811",
+
+  appId: "1:619437220811:web:96295158981c815d3fcc4e"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
