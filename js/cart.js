9d46a69393c9bd366e658391c3fecc0a028504ed
Ignacio-CI <ignacio.cerda.i@gmail.com>
2023-03-07 20:59:15 +0100

    update

--- js/cart.js
+++ js/cart.js
@@ -45,21 +45,22 @@ const cart = JSON.parse(localStorage.getItem('products'));
 const cartContainer = document.querySelector('#cart-container');
 let total = 0;
 
-cart.forEach(({name, price}) => {
+cart.forEach(({ name, price }) => {
 
-     // const itemImg = document.createElement('img');
-     // itemImg.src = `.${imgUrl}`;  
+    // const itemImg = document.createElement('img');
+    // itemImg.src = `.${imgUrl}`;  
 
     const itemName = document.createElement('h2');
     itemName.innerText = `${name}`;
 
     const itemPrice = document.createElement('h3');
     itemPrice.innerText = `${price}`;
-    
+
     cartContainer.append(itemName, itemPrice);
 
-    total += price; 
+    total += price;
     const totalValue = document.querySelector('#total-value')
+    totalValue.innerText = 'Total price :' + total
     totalValue.innerText = 'Total price :' + total
 });
 const emptyBtn = document.querySelector('#empty-btn');
