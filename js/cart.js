b8b9e41c775c7b65a8df6deec87e69a9016b98f4
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2023-03-07 19:06:12 +0100

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getDatabase, ref, get, set, onValue, update, child } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js"
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBVGKXt0navmTtlydS0X82fEUJTqxxcvg8",
+
+  authDomain: "javascript2-mp3-store.firebaseapp.com",
+
+  databaseURL: "https://javascript2-mp3-store-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "javascript2-mp3-store",
+
+  storageBucket: "javascript2-mp3-store.appspot.com",
+
+  messagingSenderId: "619437220811",
+
+  appId: "1:619437220811:web:96295158981c815d3fcc4e"
+
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+
+//Hämta products från databasen
+const productsRef = ref(database, 'products');
+
+const productsSnapshot = await get(productsRef);
+const products = productsSnapshot.val();
+
+const productIdArray = Object.keys(products);
+
+
+
 const cart = JSON.parse(localStorage.getItem('products'));
 const cartContainer = document.querySelector('#cart-container');
 let total = 0;
@@ -17,7 +60,31 @@ cart.forEach(({name, price}) => {
 
     total += price; 
     const totalValue = document.querySelector('#total-value')
-totalValue.innerText = 'Total price :' + total
+    totalValue.innerText = 'Total price :' + total
+});
+const emptyBtn = document.querySelector('#empty-btn');
+
+emptyBtn.addEventListener('click', event => {
+  event.preventDefault();
+  localStorage.removeItem('products');
+  location.reload()
 });
 
-console.log(total); 
+
+const buyBtn = document.querySelector('#pay-btn');
+buyBtn.addEventListener('click', event => {
+    event.preventDefault();
+    localStorage.removeItem('products');
+    location.reload()
+
+    const db = getDatabase();
+    productIdArray.forEach(id => {
+      update(ref(db, 'products/' + id), (snapshot) => {
+        const saldo = snapshot.val().saldo;
+        console.log(saldo) 
+         
+      })
+  
+    })
+
+  });
