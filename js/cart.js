c7fd4e44938740b7087ceb274a47c5b7fc886c99
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2023-03-07 12:42:28 +0100

    cart form

--- js/cart.js
+++ js/cart.js
@@ -2,16 +2,18 @@ const cart = JSON.parse(localStorage.getItem('products'));
 const cartContainer = document.querySelector('#cart-container');
 console.log(cart)
 
-cart.forEach(({imgUrl, name, price}) => {
-    console.log(imgUrl, name, price)
+cart.forEach(({name, price}) => {
+    // console.log(imgUrl, name, price)
 
-    const itemImg = document.createElement('img');
-    itemImg.src = `.${imgUrl}`;
+    // const itemImg = document.createElement('img');
+    // itemImg.src = `.${imgUrl}`;
 
     const itemName = document.createElement('h2');
-    itemName.innerText = `.${imgUrl}`;
+    itemName.innerText = `.${name}`;
 
     const itemPrice = document.createElement('h3');
     itemPrice.innerText = `.${price}`;
     
+    cartContainer.append( itemName, itemPrice)
+
 });
